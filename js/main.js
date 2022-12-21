627bd7ba4ca61c49697c7a37d82fba4472ca5b8b
aravindh203 <aravindhkumar218@gmail.com>
2022-12-21 21:17:28 +0530

    add

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let object={};
+object.name=prompt("enter name");
+object.age=parseInt(prompt("enter age"));
+object.profession=prompt("enter profession");
+object.Qualification=prompt("enter profession");
+object.phoneno=parseInt(prompt("enter phone number"));
+console.log(object);
